aa4b44dd45c14f0706801c1d527c90cde20f1aaa
Luka Udovicic <lukaudovicic10@gmail.com>
2023-03-22 19:36:08 +0100

    created Landing page , custom hook for fetching api information , implemented header with navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import LandingPage from "./LandingPage";
 export default function Home() {
   return (
     <>
-      <h1 className="bg-bluepb-200 text-bluepb-500 text-3xl">Hello world</h1>
+      <LandingPage />
+      <h1 className="bg-bluepb-200 text-bluepb-500 text-3xl"></h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      padding: "7.75rem",
+    },
     extend: {
       colors: {
         neutral: {
