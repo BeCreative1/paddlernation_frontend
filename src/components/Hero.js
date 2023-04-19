d7406184d94c7105446d35429775f93a367e70b8
Luka Udovicic <lukaudovicic10@gmail.com>
2023-04-19 11:42:24 +0200

    worked on the animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { RxDoubleArrowDown } from "react-icons/rx";
+import { animateScroll } from "react-scroll";
 
 const Hero = () => {
+  const handleScroll = () => {
+    animateScroll.scrollTo(window.innerHeight, {
+      duration: 1000,
+      delay: 0,
+      smooth: "easeInOutQuart",
+    });
+  };
   return (
     <div className="h-screen relative flex flex-col items-center justify-center">
-      <div className="absolute bottom-3 left-1/2 transform -translate-x-1/2 z-10 sm:bottom-12 md:bottom-10 lg:bottom-16">
-        <a href="/">
-          <RxDoubleArrowDown className="w-10 h-10 text-neutral-100" />
-        </a>
+      <div
+        className="absolute bottom-3 left-1/2 transform -translate-x-1/2 z-10 sm:bottom-12 md:bottom-10 lg:bottom-16"
+        onClick={handleScroll}
+      >
+        <RxDoubleArrowDown className="w-10 h-10 text-neutral-100" />
       </div>
       <video
         src="/assets/heroVideo_v2.mp4"
@@ -17,7 +26,7 @@ const Hero = () => {
         loop
       ></video>
       <div className="absolute sm:bottom-[150px] md:bottom-40 lg:bottom-17">
-        <a href="/">
+        <a href="/RentingPage">
           <button className="bg-neutral-100 hover:bg-neutral-200 text-neutral-700 h-12 w-48 rounded-xl">
             Booking
           </button>
