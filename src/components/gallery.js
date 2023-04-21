9844031cbf488e67e10a56f04916e8008968e998
Davidek776 <kocurdavid776@gmail.com>
2023-04-21 19:00:44 +0200

    Gsap logic works.

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,15 +1,67 @@
 import React from "react";
-import { gsap } from "gsap";
+import gsap from "gsap";
 import { useRef, useEffect } from "react";
+import { ScrollTrigger } from "node_modules/gsap/ScrollTrigger";
 
 const Gallery = () => {
+  gsap.registerPlugin(ScrollTrigger);
   const app = useRef();
   useEffect(() => {
-    let ctx = gsap.context(() => {
-      gsap.to(".square", { rotate: 360, duration: 5 });
-    }, app);
+    var tl = gsap.timeline({
+      defaults: {
+        ease: "linear",
+        repeat: -1,
+      },
+    });
 
-    return () => ctx.revert();
+    tl.fromTo(
+      ".ImageScroll__image-1",
+      { x: "-100vw" },
+      {
+        x: 0,
+        duration: 10,
+      },
+      0
+    );
+
+    tl.fromTo(
+      ".ImageScroll__image-2",
+      { x: 0 },
+      {
+        x: "-100vw",
+        duration: 10,
+      },
+      0
+    );
+
+    tl.fromTo(
+      ".ImageScroll__image-3",
+      { x: "-100vw" },
+      {
+        x: 0,
+        duration: 10,
+      },
+      0
+    );
+
+    ScrollTrigger.create({
+      trigger: "section",
+      start: "top top",
+      end: "bottom",
+      markers: true,
+      onEnter: () => {
+        tl.play();
+      },
+      onLeave: () => {
+        tl.pause();
+      },
+      onUpdate: (self) => {
+        const velocity = Math.abs(self.getVelocity());
+        tl.timeScale(velocity / 100);
+        tl.timeScale(velocity / 100);
+        gsap.to(tl, { timeScale: 1, overwrite: true });
+      },
+    });
   }, []);
   return (
     <>
@@ -93,7 +145,14 @@ const Gallery = () => {
         </div>
       </div> */}
       <div ref={app}>
-        <div className="square">Hello World</div>
+        <section>
+          <div class="ImageScroll__wrapper">
+            <div class="ImageScroll__image    ImageScroll__image-1"></div>
+          </div>
+          <div class="ImageScroll__wrapper">
+            <div class="ImageScroll__image    ImageScroll__image-2"></div>
+          </div>
+        </section>
       </div>
     </>
   );
