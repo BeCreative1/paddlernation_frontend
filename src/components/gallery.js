f9025656d50d166bafeee5e732809b134b698d88
Davidek776 <kocurdavid776@gmail.com>
2023-05-05 19:38:12 +0200

    Add images and add styling.

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -45,16 +45,17 @@ const Gallery = () => {
     );
 
     ScrollTrigger.create({
-      trigger: "section",
+      trigger: "body",
       start: "top top",
       end: "bottom",
-      markers: true,
-      onEnter: () => {
-        tl.play();
-      },
-      onLeave: () => {
-        tl.pause();
-      },
+      markers: false,
+
+      // onEnter: () => {
+      //   tl.play();
+      // },
+      // onLeave: () => {
+      //   tl.pause();
+      // },
       onUpdate: (self) => {
         const velocity = Math.abs(self.getVelocity());
         tl.timeScale(velocity / 100);
@@ -145,16 +146,21 @@ const Gallery = () => {
         </div>
       </div> */}
       <div ref={app}>
-        <section>
-          <div className="ImageScroll__wrapper flex content-center">
-            <div className="ImageScroll__image    ImageScroll__image-1 flex">
+        <section className="">
+          <div className="ImageScroll__wrapper hidden content-center mb-10 mt-24 md:flex ">
+            <div className="ImageScroll__image    ImageScroll__image-1 flex ">
               <img
                 src="./assets/GallerySectionImages/1.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
               <img
-                src="./assets/GallerySectionImages/2.jpg"
+                src="./assets/GallerySectionImages/27.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
@@ -164,7 +170,22 @@ const Gallery = () => {
                 alt=""
               />
               <img
+                src="./assets/GallerySectionImages/25.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
                 src="./assets/GallerySectionImages/4.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/24.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
@@ -174,7 +195,32 @@ const Gallery = () => {
                 alt=""
               />
               <img
+                src="./assets/GallerySectionImages/23.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/2.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/26.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
                 src="./assets/GallerySectionImages/6.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/22.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
@@ -184,7 +230,22 @@ const Gallery = () => {
                 alt=""
               />
               <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/21.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
                 src="./assets/GallerySectionImages/8.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/20.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
@@ -194,7 +255,27 @@ const Gallery = () => {
                 alt=""
               />
               <img
+                src="./assets/GallerySectionImages/19.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
                 src="./assets/GallerySectionImages/10.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/18.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
@@ -204,26 +285,92 @@ const Gallery = () => {
                 alt=""
               />
               <img
+                src="./assets/GallerySectionImages/17.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
                 src="./assets/GallerySectionImages/12.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/19.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/13.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/18.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/14.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/17.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/15.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/16.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
             </div>
           </div>
-          <div className="ImageScroll__wrapper">
-            <div className="ImageScroll__image    ImageScroll__image-2 flex">
+          <div className="ImageScroll__wrapper mt-24 mb-24 md:mt-0">
+            <div className="ImageScroll__image ImageScroll__image-2 flex ">
               <img
                 src="./assets/GallerySectionImages/1.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
               <img
-                src="./assets/GallerySectionImages/2.jpg"
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw] hidden md:block"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/27.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+
+              <img
+                src="./assets/GallerySectionImages/3.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
               <img
-                src="./assets/GallerySectionImages/3.jpg"
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]  hidden md:block"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/25.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
@@ -233,7 +380,32 @@ const Gallery = () => {
                 alt=""
               />
               <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]  hidden md:block"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/24.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
                 src="./assets/GallerySectionImages/5.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/23.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/2.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/26.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
@@ -243,7 +415,22 @@ const Gallery = () => {
                 alt=""
               />
               <img
+                src="./assets/GallerySectionImages/22.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]  hidden md:block"
+                alt=""
+              />
+              <img
                 src="./assets/GallerySectionImages/7.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/21.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
@@ -253,12 +440,32 @@ const Gallery = () => {
                 alt=""
               />
               <img
+                src="./assets/GallerySectionImages/20.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
                 src="./assets/GallerySectionImages/9.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
               <img
+                src="./assets/GallerySectionImages/19.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]  hidden md:block"
+                alt=""
+              />
+              <img
                 src="./assets/GallerySectionImages/10.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/18.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
@@ -268,7 +475,77 @@ const Gallery = () => {
                 alt=""
               />
               <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]  hidden md:block"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]  hidden md:block"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/17.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
                 src="./assets/GallerySectionImages/12.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]  hidden md:block"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/19.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/13.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]  hidden md:block"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/18.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/14.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]  hidden md:block"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/17.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/15.jpg"
+                className=" mr-[1vw]"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/White.jpg"
+                className=" mr-[1vw]  hidden md:block"
+                alt=""
+              />
+              <img
+                src="./assets/GallerySectionImages/16.jpg"
                 className=" mr-[1vw]"
                 alt=""
               />
